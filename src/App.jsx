6840566dd73e2e19059dc6ccb0f64ce407020203
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-04-17 02:08:45 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WordApp from './components/WordApp';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="container-xl">
-      <div className='row justify-content-center'>
-        <div className='col-8'>
-          <div className='d-flex justify-content-center'>
-            <WordApp />
+    <>
+      <Nav />
+      <div className='container-xl'>
+        <div className='row justify-content-center'>
+          <div className='col-8'>
+            <div className='d-flex justify-content-center'>
+              <WordApp />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Nav() {
   return (
     <>
-      
+      <div>1</div>
+      <div>2</div>
+      <div className='border-bottom'>3</div>
     </>
   );
 }
