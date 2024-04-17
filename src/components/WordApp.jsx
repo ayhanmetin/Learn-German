c272c3f32cd134d0bfcd7299319fd812e9be9261
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-04-18 00:06:01 +0200

    update

--- src/components/WordApp.jsx
+++ src/components/WordApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import wordData from '../wordData';
 import './wordApp.css';
@@ -6,9 +6,27 @@ import './wordApp.css';
 function WordApp() {
   const [visibleWordsCount, setVisibleWordsCount] = useState(5);
 
+  useEffect(() => {
+    if (window.speechSynthesis.onvoiceschanged !== undefined) {
+      window.speechSynthesis.onvoiceschanged = populateVoices;
+    }
+  }, []);
+
+  // Function to populate voices and set a default female voice if available
+
+  const populateVoices = () => {
+    let voices = window.speechSynthesis.getVoices();
+    return voices.filter(voice => voice.gender === 'female' || voice.name.toLowerCase().includes('female'));
+  };
+
   // Function to handle reading the word out loud
+
   const readWordAloud = (text) => {
     const speech = new SpeechSynthesisUtterance(text);
+    let voices = populateVoices();
+    if (voices.length > 0) {
+      speech.voice = voices[0];  // Set to the first female voice found
+    }
     window.speechSynthesis.speak(speech);
   };
 
@@ -22,8 +40,8 @@ function WordApp() {
         <div className='border-bottom mb-4' key={word.id}>
           <p className=''>
             <b>Word of the Day:</b> {word.word}
-            <span onClick={() => readWordAloud(word.word)} style={{ cursor: 'pointer' }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-volume-up-fill" viewBox="0 0 16 16" style={{ marginLeft: '5px' }}>
+            <span onClick={() => readWordAloud(word.word)} style={{ cursor: 'pointer', marginLeft: '5px' }}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-volume-up-fill" viewBox="0 0 16 16">
                 <path d="M11.536 14.01A8.47 8.47 0 0 0 14.026 8a8.47 8.47 0 0 0-2.49-6.01l-.708.707A7.48 7.48 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303z"/>
                 <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.48 5.48 0 0 1 11.025 8a5.48 5.48 0 0 1-1.61 3.89z"/>
                 <path d="M8.707 11.182A4.5 4.5 0 0 0 10.025 8a4.5 4.5 0 0 0-1.318-3.182L8 5.525A3.5 3.5 0 0 1 9.025 8 3.5 3.5 0 0 1 8 10.475zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06"/>
@@ -33,7 +51,7 @@ function WordApp() {
           <p>
             <b>Date:</b> {word.date}
           </p>
-          <h2 className=''>
+          <h2>
             {word.word}
           </h2>
           <p>
