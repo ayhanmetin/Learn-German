745db355406cfa43134070f5eb3a0d9a1259f650
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-04-18 13:42:48 +0200

    update

--- src/components/WordApp.jsx
+++ src/components/WordApp.jsx
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import wordData from '../wordData';
 import './wordApp.css';
+import { VoiceIcon } from './IconBox';
+
 
 function WordApp() {
-  // State for controlling the visibility count of words
+
+
   const [visibleWordsCount, setVisibleWordsCount] = useState(5);
-  // State for handling the search term input
   const [searchTerm, setSearchTerm] = useState('');
+  const [filteredWords, setFilteredWords] = useState([]);
 
   // Effect hook for setting up speech synthesis when voices change
   useEffect(() => {
@@ -59,25 +62,29 @@ function WordApp() {
     printWindow.document.close();
 
     // Wait for the print dialog to close
-    printWindow.onafterprint = function() {
-        printWindow.close(); // Close the print window
-        window.scrollTo(0, currentScrollPosition); // Restore the scroll position
+
+    printWindow.onafterprint = function () {
+      printWindow.close();
+      window.scrollTo(0, currentScrollPosition); // Restore the scroll position
     };
 
     printWindow.print();
-};
+  };
 
-
-  // Filtering logic to find words based on the search term or display a limited number
-  const filteredWords =
-    searchTerm.length > 0
-      ? wordData.filter(
-          word =>
-            word.word.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            word.meaningENG.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            word.meaningTR.toLowerCase().includes(searchTerm.toLowerCase())
+  useEffect(() => {
+    const newFilteredWords = searchTerm.trim().length > 0
+      ? wordData.filter(word =>
+          word.word.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          word.meaningENG.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          word.meaningTR.toLowerCase().includes(searchTerm.toLowerCase())
         )
       : wordData.slice(0, visibleWordsCount);
+    setFilteredWords(newFilteredWords);
+  }, [searchTerm, visibleWordsCount]);
+
+
+
+
 
   return (
     <div className='container col-12'>
@@ -110,18 +117,8 @@ function WordApp() {
                 onClick={() => readWordAloud(word)}
                 style={{ cursor: 'pointer', marginLeft: '5px' }}
               >
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='46'
-                  height='36'
-                  fill='currentColor'
-                  className='bi bi-volume-up-fill'
-                  viewBox='0 0 16 16'
-                >
-                  <path d='M11.536 14.01A8.47 8.47 0 0 0 14.026 8a8.47 8.47 0 0 0-2.49-6.01l-.708.707A7.48 7.48 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303z' />
-                  <path d='M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.48 5.48 0 0 1 11.025 8a5.48 5.48 0 0 1-1.61 3.89z' />
-                  <path d='M8.707 11.182A4.5 4.5 0 0 0 10.025 8a4.5 4.5 0 0 0-1.318-3.182L8 5.525A3.5 3.5 0 0 1 9.025 8 3.5 3.5 0 0 1 8 10.475zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06' />
-                </svg>
+                <VoiceIcon width="30" height="30" />
+                
               </span>
             </div>
           </div>
