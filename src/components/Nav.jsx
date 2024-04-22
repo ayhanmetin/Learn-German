1d16840d1e9c053aea0c665955f2c738c598d3fd
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-04-23 01:07:38 +0200

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,76 +5,44 @@ import Profile from './Profile';
 import Theme2 from './Theme2';
 import { NavLink } from 'react-router-dom';
 import { BookmarkedIcon, HomeIcon, MixIcon } from './IconBox';
-// import WordCountDisplay from './WordCountDisplay';
-// import wordData from '../wordData';
 
 export default function Nav() {
   return (
     <>
-      <div className='container mb-5 pb-2'>
+      <div className='container mb-2 pb-2'>
         <div
-          className='d-flex justify-content-between align-items-center rounded-bottom gap-3 px-3'
-          style={{
-            backgroundColor: '#DA513D',
-          }}
+          className='container p-2 mb-1 pb-2 d-flex justify-content-between align-items-center rounded-bottom gap-3 px-3'
+          style={{ backgroundColor: '#DA513D' }}
         >
-          <div className='profilLink'>
-            <NavLink to='/profile' className=''>
-              <Profile />
-            </NavLink>
-          </div>
+          <NavLink to='/profile' className='profile-link'>
+            <Profile />
+          </NavLink>
 
-          <div className='d-flex navLink justify-content-center text-light align-items-center gap-5'>
-            <NavLink to='/home' className='text-light'>
+          <div className='navigation-links d-flex justify-content-center text-light align-items-center gap-5'>
+            <NavLink to='/home' className='text-light' aria-label='Home'>
               <HomeIcon />
             </NavLink>
-            <NavLink to='/mix' className='text-light'>
+            <NavLink to='/mix' className='text-light' aria-label='Mix'>
               <MixIcon />
             </NavLink>
-            <NavLink to='/favorites' className='text-light'>
+            <NavLink
+              to='/favorites'
+              className='text-light'
+              aria-label='Bookmarked'
+            >
               <BookmarkedIcon />
             </NavLink>
           </div>
 
-          <div className='position-relative' style={{ top: '-3px' }}>
+          <div>
             <Theme2 />
           </div>
         </div>
-        <div className='learnGerman mt-2 fw-light text-body-emphasis'>
+
+        <div className='learn-german mt-2 fw-light text-body-emphasis'>
           Deutsch lernen für TestDaF, Goethe-Institut und TELC-Zertifizierung
         </div>
       </div>
-
-      {/* <div className='container mt-5'>
-        <ul className='navbar-nav me-auto mb-3'>
-          <div className='d-flex gap-5 justify-content-center'>
-            <li className='nav-item'>
-              <NavLink
-                className='favButton link-warning link-offset-3 link-underline-opacity-25 link-underline-opacity-100-hover'
-                to='/'
-              >
-                <HomeIcon />
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink
-                className='favButton link-warning link-offset-3 link-underline-opacity-100 link-underline-secondary link-underline-opacity-100-hover'
-                to='/mix'
-              >
-                <MixIcon />
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink
-                className='favButton link-warning link-offset-3 link-underline-opacity-100 link-underline-secondary link-underline-opacity-100-hover'
-                to='/favorites'
-              >
-                <BookmarkedIcon />
-              </NavLink>
-            </li>
-          </div>
-        </ul>
-      </div> */}
     </>
   );
 }
