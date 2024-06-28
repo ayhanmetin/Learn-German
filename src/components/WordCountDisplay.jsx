8129686825a5fa04293a9dac11ed79fbeca3ff76
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-06-29 01:12:51 +0200

    update

--- src/components/WordCountDisplay.jsx
+++ src/components/WordCountDisplay.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import wordData from '../wordData'; 
+import wordData from '../wordData';
 
 const WordCountDisplay = () => {
-  const wordCount = wordData.length; 
+  const wordCount = wordData.length;
 
   return (
     <div>
-      <h3 className='wordCount ms-3'>Total words: {wordCount}</h3> 
+      <h3 className='wordCount ms-3 mt-3'>Total words: {wordCount}</h3>
     </div>
   );
 };
