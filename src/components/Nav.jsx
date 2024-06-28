c131755799ff00ce541226142c25b207a9207516
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-06-29 01:11:39 +0200

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,38 +16,30 @@ export default function Nav() {
   return (
     <>
       <nav
-        className='d-flex justify-content-between align-items-center rounded-bottom gap-1 px-3'
-        style={{ backgroundColor: '#132732' }}
-      >
-        <NavLink
-          to='/calendar'
-          className='text-white calIcon'
-          aria-label='Calendar'
-        >
-          <CalIcon />
-        </NavLink>
-        <div className='d-flex navLink gap-0 justify-content-center align-items-center'>
-          <NavLink
-            to='/home'
-            className='text-light navLink me-5'
-            aria-label='Home'
-          >
-            <HomeIcon />
-          </NavLink>
-          {/* Uncomment or modify these as needed for other navigation links */}
-          {/* <NavLink
-            to='/favorites'
-            className='text-light navLink'
-            aria-label='Favorites'
-          >
-            <BookmarkedIcon />
-          </NavLink>
-          <NavLink to='/blog' className='text-light navLink' aria-label='Blog'>
-            <BlogIcon />
-          </NavLink> */}
-        </div>
-        <Theme2 />
-      </nav>
+  className='d-flex justify-content-between align-items-center rounded-bottom px-3'
+  style={{ backgroundColor: '#132732', height: '50px' }}
+>
+  <NavLink
+    to='/calendar'
+    className='text-white'
+    aria-label='Calendar'
+    style={{ flex: 1, display: 'flex', justifyContent: 'center' }}
+  >
+    <CalIcon />
+  </NavLink>
+  <NavLink
+    to='/home'
+    className='text-light'
+    aria-label='Home'
+    style={{ flex: 1, display: 'flex', justifyContent: 'center' }}
+  >
+    <HomeIcon />
+  </NavLink>
+  <div style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
+    <Theme2 />
+  </div>
+</nav>
+
 
       <div className='ms-0 ps-0 mb-0 pb-0'>
         <div className='d-flex altNav align-items-center justify-content-center p-0 mt-5 mb-0'>
@@ -66,26 +58,10 @@ export default function Nav() {
             >
               #advanced
             </NavLink>
-            {/* Additional links for travel, quiz, etc., can be added here */}
-            {/* <NavLink
-              to='/travel'
-              aria-label='Explore Advanced German Words'
-              className='altNav2'
-            >
-              #travel
-            </NavLink>
-            <NavLink
-              to='/quiz'
-              aria-label='Explore Mixed German Words'
-              className='altNav2 quiz-button'
-            >
-              #quiz
-            </NavLink> */}
           </nav>
         </div>
       </div>
       <WordCountDisplay />
-      <div className='border-bottom border-success p-2 border-opacity-25'></div>
     </>
   );
 }
