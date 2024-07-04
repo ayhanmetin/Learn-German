f1022e67c153ef79db065fdeacaba06395512f62
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-07-04 02:22:48 +0200

    update

--- src/Categories/Quiz.jsx
+++ src/Categories/Quiz.jsx
@@ -157,7 +157,7 @@ const Quiz = () => {
         )}
         {wrongWords.length > 0 && (
           <div className='wrong-words text-body-secondary ms-4 mb-5 pb-5'>
-            <h3>🏴‍☠️ {wrongWords.join(', ')}</h3>
+            <h3 className='fs-5'>🏴‍☠️ {wrongWords.join(', ')}</h3>
           </div>
         )}
       </div>
