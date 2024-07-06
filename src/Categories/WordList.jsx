20842e8b69a0e48c7dd6399b7e198e1922511cd8
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-07-06 16:10:41 +0200

    update

--- src/Categories/WordList.jsx
+++ src/Categories/WordList.jsx
@@ -1,68 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 import wordData from '../wordData';
 import './wordList.css';
 
 const WordList = () => {
   const navigate = useNavigate();
-  const [selectedGrammar, setSelectedGrammar] = useState('total');
-  const [filteredWords, setFilteredWords] = useState([]);
-
-  const grammarCounts = {
-    total: wordData.length,
-    adverb: wordData.filter(word => word.grammar === 'adverb').length,
-    verb: wordData.filter(word => word.grammar === 'verb').length,
-    others: wordData.filter(
-      word => word.grammar !== 'adverb' && word.grammar !== 'verb'
-    ).length,
-  };
-
-  useEffect(() => {
-    if (selectedGrammar === 'total') {
-      setFilteredWords(wordData);
-    } else {
-      setFilteredWords(
-        wordData.filter(word => word.grammar === selectedGrammar)
-      );
-    }
-  }, [selectedGrammar]);
 
   const handleWordClick = word => {
     navigate(`/word/${encodeURIComponent(word)}`);
   };
 
-  const handleCategoryClick = category => {
-    setSelectedGrammar(category);
-  };
-
   return (
     <div className='col-11 mt-0 pt-0 mb-5'>
-      <div>
-        {/* <span
-          className='wordCountx fs-5'
-          style={{ cursor: 'pointer' }}
-          onClick={() => handleCategoryClick('adverb')}
-        >
-          Adverbs: {grammarCounts.adverb}
-        </span>
-        
-        <span
-          className='wordCountx fs-5'
-          style={{ cursor: 'pointer' }}
-          onClick={() => handleCategoryClick('verb')}
-        >
-          Verbs: {grammarCounts.verb}
-        </span> */}
-        {/* <span
-          className='wordCountx fs-5'
-          style={{ cursor: 'pointer' }}
-          onClick={() => handleCategoryClick('others')}
-        >
-          Others: {grammarCounts.others}
-        </span> */}
-      </div>
-      <div className='word-list mt-4'>
-        {filteredWords.map((word, index) => (
+      <div className='word-list'>
+        {wordData.map((word, index) => (
           <React.Fragment key={index}>
             <span
               className='wordList'
@@ -72,7 +23,7 @@ const WordList = () => {
               {word.article ? `${word.article} ` : ''}
               {word.word}
             </span>
-            {index < filteredWords.length - 1 && <span>, </span>}
+            {index < wordData.length - 1 && <span>, </span>}
           </React.Fragment>
         ))}
       </div>
