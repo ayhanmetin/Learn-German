492c607d46409d9e8057fb44bf54428c6192c63a
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-07-12 01:53:51 +0200

    update

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -16,20 +16,38 @@ const Today = () => {
     setWordDetails(filteredWord);
   }, [wordNo]);
 
-  const navigateDate = useCallback((direction) => {
-    const currentDate = wordNo ? new Date(2000 + parseInt(wordNo.slice(4, 6)), parseInt(wordNo.slice(2, 4)) - 1, parseInt(wordNo.slice(0, 2))) : new Date();
-    currentDate.setDate(currentDate.getDate() + (direction === 'future' ? 1 : -1));
-    const newWordNo = `${currentDate.getDate().toString().padStart(2, '0')}${(currentDate.getMonth() + 1).toString().padStart(2, '0')}${currentDate.getFullYear().toString().slice(2)}`;
-    navigate(`/today/${newWordNo}`);
-  }, [wordNo, navigate]);
+  const navigateDate = useCallback(
+    direction => {
+      const currentDate = wordNo
+        ? new Date(
+            2000 + parseInt(wordNo.slice(4, 6)),
+            parseInt(wordNo.slice(2, 4)) - 1,
+            parseInt(wordNo.slice(0, 2))
+          )
+        : new Date();
+      currentDate.setDate(
+        currentDate.getDate() + (direction === 'future' ? 1 : -1)
+      );
+      const newWordNo = `${currentDate.getDate().toString().padStart(2, '0')}${(
+        currentDate.getMonth() + 1
+      )
+        .toString()
+        .padStart(2, '0')}${currentDate.getFullYear().toString().slice(2)}`;
+      navigate(`/today/${newWordNo}`);
+    },
+    [wordNo, navigate]
+  );
 
   const copyUrlToClipboard = useCallback(() => {
     const url = `${window.location.origin}${location.pathname}`;
-    navigator.clipboard.writeText(url).then(() => {
-      console.log('URL copied to clipboard!');
-    }).catch(err => {
-      console.error('Failed to copy: ', err);
-    });
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        console.log('URL copied to clipboard!');
+      })
+      .catch(err => {
+        console.error('Failed to copy: ', err);
+      });
   }, [location.pathname]);
 
   const ShareButton = () => {
@@ -56,35 +74,21 @@ const Today = () => {
     );
   };
 
-  const readWordAloud = (word) => {
-    console.log("Reading the word aloud: ", word);
-    // Add actual implementation to read word aloud
-  };
-
   return (
     <div className='col-12'>
-      <div className='d-flex justify-content-between'>
-        <button onClick={() => navigateDate('past')}>Previous Day</button>
-        <ShareButton />
-        <button onClick={() => navigateDate('future')}>Next Day</button>
+      <div className='d-flex justify-content-evenly mb-5'>
+        <button onClick={() => navigateDate('past')}>Previous</button>
+        <div className='mt-1 ms-4'>
+          <ShareButton />
+        </div>
+        <button onClick={() => navigateDate('future')}>Next</button>
       </div>
-      <div className='d-flex justify-content-start align-items-center'>
-        <div className='col-md-12'>
+      <div className='d-flex justify-content-start align-items-center border-top'>
+        <div className='col-md-12 mt-5'>
           <div className='d-flex justify-content-start align-items-center mb-3'>
             {wordDetails ? (
               <>
                 <b className='mobileWord wordDay fs-1'>{`${wordDetails.article} ${wordDetails.word}`}</b>
-                <button
-                  className='ms-2 position-relative'
-                  onClick={() => readWordAloud(wordDetails)}
-                  style={{
-                    border: 'none',
-                    background: 'transparent',
-                    cursor: 'pointer',
-                  }}
-                >
-                  <VoiceIcon width='22' height='22' />
-                </button>
               </>
             ) : (
               <b className='fs-1'>No word for this date</b>
@@ -98,16 +102,22 @@ const Today = () => {
                     {wordDetails.grammar}
                   </p>
                 )}
-                {[1, 2, 3, 4, 5].map(i => wordDetails[`example${i}`] && (
-                  <p className='textWord' key={`example${i}`}>
-                    <strong>‣</strong> {wordDetails[`example${i}`]}
-                  </p>
-                ))}
-                {[1, 2, 3].map(i => wordDetails[`tip${i}`] && (
-                  <p className='textWord mt-3' key={`tip${i}`}>
-                    <strong>&nbsp;⇢</strong> {wordDetails[`tip${i}`]}
-                  </p>
-                ))}
+                {[1, 2, 3, 4, 5].map(
+                  i =>
+                    wordDetails[`example${i}`] && (
+                      <p className='textWord' key={`example${i}`}>
+                        <strong>‣</strong> {wordDetails[`example${i}`]}
+                      </p>
+                    )
+                )}
+                {[1, 2, 3].map(
+                  i =>
+                    wordDetails[`tip${i}`] && (
+                      <p className='textWord mt-3' key={`tip${i}`}>
+                        <strong>&nbsp;⇢</strong> {wordDetails[`tip${i}`]}
+                      </p>
+                    )
+                )}
                 <p className='textWord meaning fst-italic mt-4'>
                   <strong>&nbsp;-</strong> {wordDetails.meaningENG}
                 </p>
@@ -120,7 +130,10 @@ const Today = () => {
             </div>
           ) : (
             <div className='text-center fs-4'>
-              <p>No word details available for this date. Try navigating to another day!</p>
+              <p>
+                No word details available for this date. Try navigating to
+                another day!
+              </p>
             </div>
           )}
         </div>
