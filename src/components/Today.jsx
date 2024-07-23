5eaad3d3eb25de0ce5cc78cd055ea2aaa738de51
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-07-23 19:45:05 +0200

    update

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -76,7 +76,7 @@ const Today = () => {
     <div className='col-10'>
       <div className='d-flex p-5 todayButton justify-content-evenly mb-5'>
         <button
-          className='btn px-4 btn homeCss'
+          className='btn fs-6 px-4 btn homeCss'
           onClick={() => navigateDate('past')}
         >
           Previous
@@ -85,7 +85,7 @@ const Today = () => {
           <ShareButton />
         </div>
         <button
-          className='btn px-5 homeCss'
+          className='btn fs-6 px-5 homeCss'
           onClick={() => navigateDate('future')}
         >
           Next
