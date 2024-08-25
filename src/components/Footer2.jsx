24e21bae07c4c17ac72d82169cb47a476bb065fd
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-08-25 21:24:58 +0200

    update

--- src/components/Footer2.jsx
+++ src/components/Footer2.jsx
@@ -8,26 +8,13 @@ export default function Footer2() {
 
   return (
     <>
-      <div className='my-custom-margin mt-5 pt-5 border-top'>
+      <div className='my-custom-margin mt-5 pt-0'>
         <footer className='container d-flex flex-column flex-sm-row justify-content-between align-items-center pt-1 mt-0'>
           <ul className='list-unstyled mb-0 w-100 text-center d-flex flex-column'>
             <li>
               <p className='mb-1 mt-5'>© 2024 Ayhan Metin</p>
               <div className='d-flex justify-content-center align-items-center'>
-                {/* <p className='mb-5 me-3 mt-1 text-body-secondary'>
-                  <a
-                    href={xUrl}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    style={{
-                      border: 'none',
-                      background: 'none',
-                      cursor: 'pointer',
-                    }}
-                  >
-                    <XIcon />
-                  </a>
-                </p> */}
+                
               </div>
             </li>
           </ul>
