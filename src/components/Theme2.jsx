03db18c61322e0cc94b0fa74089ceefdbb5a0ff3
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-08-25 21:49:28 +0200

    update

--- src/components/Theme2.jsx
+++ src/components/Theme2.jsx
@@ -18,7 +18,7 @@ const darkThemeIcon = (
     xmlns='http://www.w3.org/2000/svg'
     width='20'
     height='20'
-    fill='currentColor'
+    fill='white'
     className='bi bi-toggle-off '
     viewBox='0 0 16 16'
   >
