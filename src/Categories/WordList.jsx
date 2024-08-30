20b707c0dce3cb64e8bdf0908dee979c01b63c71
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-08-31 01:14:19 +0200

    update

--- src/Categories/WordList.jsx
+++ src/Categories/WordList.jsx
@@ -13,7 +13,7 @@ const WordList = () => {
 
   return (
     <>
-      <div className='d-flex px-1 align-items-center justify-content-center mb-5 word-list'>
+      <div className='d-flex px-2 align-items-center justify-content-center mb-5 word-list'>
         <div className='text-center p-1 mt-3'>
           {wordData.map((word, index) => (
             <React.Fragment key={index}>
