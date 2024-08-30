039eb2352238b1b26bdc86d0edfd57fb96e86445
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-08-30 23:06:53 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,10 @@ import { Analytics } from '@vercel/analytics/react';
 
 import Nav from './components/Nav';
 import WordApp from './components/WordApp';
-import Advanced from './Categories/Advanced';
-import Today from './components/Today';
 import Quiz from './Categories/Quiz';
 import WordList from './Categories/WordList';
 import WordDetail from './Categories/WordDetail';
-import Home from './components/Home';
-import TestGame from './components/TestGame';
-import Grammar from './components/Grammar';
+
 import './app.css';
 
 function App() {
@@ -23,16 +19,12 @@ function App() {
           <Nav />
           <Analytics />
           <Routes>
-            <Route path='/grammar' element={<Grammar />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/' element={<Home />} />
-            <Route path='/advanced' element={<Advanced />} />
+            <Route path='/home' element={<WordApp />} />
+            <Route path='/' element={<WordApp />} />
             <Route path='/basics' element={<WordApp />} />
             <Route path='/word/:wordName' element={<WordDetail />} />
             <Route path='/word' element={<WordList />} />
-            <Route path='/test' element={<TestGame />} />
             <Route path='/quiz' element={<Quiz />} />
-            <Route path='/today/:wordNo' element={<Today />} />
           </Routes>
         </div>
       </div>
