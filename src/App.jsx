16f54afb35f1c357986155c4cd80f9122275ff75
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-09-04 16:23:52 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
@@ -14,22 +14,20 @@ import './app.css';
 
 function App() {
   return (
-    <Router>
-      <div className='d-flex justify-content-center align-items-center '>
-        <div className='col-12 col-md-8'>
-          <Nav />
-          <Analytics />
-          <SpeedInsights />
-          <Routes>
-            <Route path='/home' element={<WordApp />} />
-            <Route path='/' element={<WordApp />} />
-            <Route path='/word/:wordName' element={<WordDetail />} />
-            <Route path='/word' element={<WordList />} />
-            <Route path='/quiz' element={<Quiz />} />
-          </Routes>
-        </div>
+    <div className='d-flex justify-content-center align-items-center '>
+      <div className='col-12 col-md-8'>
+        <Nav />
+        <Analytics />
+        <SpeedInsights />
+        <Routes>
+          <Route path='/home' element={<WordApp />} />
+          <Route path='/' element={<WordApp />} />
+          <Route path='/word/:wordName' element={<WordDetail />} />
+          <Route path='/word' element={<WordList />} />
+          <Route path='/quiz' element={<Quiz />} />
+        </Routes>
       </div>
-    </Router>
+    </div>
   );
 }
 
